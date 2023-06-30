bd636d13a94dccf76d7d33d3378fa44a4c049f0c
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-30 14:33:57 +0530

    [sanjana] Add : delete contact

--- Address_Book/AddressBookOperations.cs
+++ Address_Book/AddressBookOperations.cs
@@ -9,8 +9,10 @@ namespace Address_Book
     internal class AddressBookOperations
     {
         Contact contact = new Contact();
+        List<Contact> Data = new List<Contact>();
         public void Add()
         {
+            Contact contact = new Contact();
             Console.WriteLine("Enter First Name");
             contact.FirstName = Console.ReadLine();
             Console.WriteLine("Enter Last Name");
@@ -27,59 +29,90 @@ namespace Address_Book
             contact.PhoneNumber = Console.ReadLine();
             Console.WriteLine("Enter email");
             contact.Email = Console.ReadLine();
+            Data.Add(contact);
         }
+
         public void Display()
         {
-            Console.WriteLine("First Name :" + contact.FirstName + "\n" + "Last Name :" + contact.LastName + "\n" + "Address :" + contact.Address + "\n" + "City :" + contact.City + "\n" + "State :" + contact.State + "\n" + "Zip Code :" + contact.zip + "\n" + "Phone Number :" + contact.PhoneNumber + "\n" + "Mail Id :" + contact.Email);
+            foreach (var data in Data)
+            {
+                Console.WriteLine("First Name :" + data.FirstName + "\n" + "Last Name :" + data.LastName + "\n" + "Address :" + data.Address + "\n" + "City :" + data.City + "\n" + "State :" + data.State + "\n" + "Zip Code :" + data.zip + "\n" + "Phone Number :" + data.PhoneNumber + "\n" + "Mail Id :" + data.Email);
+            }
         }
         public void Edit()
         {
-            Console.WriteLine("Select Option to edit Contct details \n 1. Edit First Name \n 2.Edit Last Name \n 3.Edit Address \n 4.Edit City \n 5.Edit State \n 6. Zip \n 7. Edit Phone Number \n 8. Edit Email ");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
+            Console.WriteLine("Edit Using First Name");
+            string name = Console.ReadLine();
+            foreach (var data in Data)
             {
-                case 1:
-                    Console.WriteLine("Enter First Name");
-                    contact.FirstName = Console.ReadLine();
-                    Display();
-                    break;
-                case 2:
-                    Console.WriteLine("Enter last Name");
-                    contact.LastName = Console.ReadLine();
-                    Display();
-                    break;
-                case 3:
-                    Console.WriteLine("Enter Address");
-                    contact.Address = Console.ReadLine();
-                    Display();
-                    break;
-                case 4:
-                    Console.WriteLine("Enter City");
-                    contact.City = Console.ReadLine();
-                    Display();
-                    break;
-                case 5:
-                    Console.WriteLine("Enter State");
-                    contact.State = Console.ReadLine();
-                    Display();
-                    break;
-                case 6:
-                    Console.WriteLine("Enter Zip Code");
-                    contact.zip = Console.ReadLine();
-                    Display();
-                    break;
-                case 7:
-                    Console.WriteLine("Enter Phone Number");
-                    contact.PhoneNumber = Console.ReadLine();
-                    Display();
-                    break;
-                case 8:
-                    Console.WriteLine("Enter Mail Id");
-                    contact.Email = Console.ReadLine();
-                    Display();
-                    break;
+                if (data.FirstName == name)
+                {
+                    Console.WriteLine("Select Option to edit Contct details \n 1. Edit First Name \n 2.Edit Last Name \n 3.Edit Address \n 4.Edit City \n 5.Edit State \n 6. Zip \n 7. Edit Phone Number \n 8. Edit Email ");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter First Name");
+                            data.FirstName = Console.ReadLine();
+                            Display();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter last Name");
+                            data.LastName = Console.ReadLine();
+                            Display();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter Address");
+                            data.Address = Console.ReadLine();
+                            Display();
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter City");
+                            data.City = Console.ReadLine();
+                            Display();
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter State");
+                            data.State = Console.ReadLine();
+                            Display();
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter Zip Code");
+                            data.zip = Console.ReadLine();
+                            Display();
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter Phone Number");
+                            data.PhoneNumber = Console.ReadLine();
+                            Display();
+                            break;
+                        case 8:
+                            Console.WriteLine("Enter Mail Id");
+                            data.Email = Console.ReadLine();
+                            Display();
+                            break;
 
+
+                    }
+                }
+            }
+
+        }
+
+        public void Delete()
+        {
+            Contact contact = new Contact();
+            Console.WriteLine("Enter First Name To delete Contact");
+            string name = Console.ReadLine();
+            foreach (var data in Data)
+            {
+                if (data.FirstName.Equals(name))
+                {
+                    contact = data;
+                }
 
             }
+            Data.Remove(contact);
         }
+    }
 }
--- Address_Book/Program.cs
+++ Address_Book/Program.cs
@@ -12,10 +12,36 @@ namespace Address_Book
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book ");
-            AddressBookOperations addressBook = new AddressBookOperations();
-            addressBook.Add();
-            addressBook.Display();
-            addressBook.Edit();
+           AddressBookOperations addressbook = new AddressBookOperations();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Option \n1. Add Contact\n 2. Edit Contact\n 3. Delete Contact\n 4.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the number of contacts to add");
+                        int N = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < N; i++)
+                        {
+                            addressbook.Add();
+                        }
+                        addressbook.Display();
+                        break;
+                    case 2:
+                        addressbook.Edit();
+                        break;
+                    case 3:
+                        addressbook.Delete();
+                        addressbook.Display();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
+    
 }

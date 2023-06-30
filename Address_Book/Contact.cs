e7f0685e74377a238ea869025eb24998a4de2368
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-30 14:19:28 +0530

    [sanjana] Add : create contact

--- Address_Book/Contact.cs
+++ Address_Book/Contact.cs
@@ -8,5 +8,13 @@ namespace Address_Book
 {
     internal class Contact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
--- Address_Book/Program.cs
+++ Address_Book/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book ");
+            Contact contact = new Contact();
+            Console.WriteLine("Enter First Name");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter zip");
+            contact.zip = Console.ReadLine();
+            Console.WriteLine("Enter PhoneNumber");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter email");
+            contact.Email = Console.ReadLine();
+
+        }
+    }
+}
